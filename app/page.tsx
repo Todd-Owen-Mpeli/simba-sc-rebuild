2c2aa3ee88245ea90cac6564b2273b8659d0a62f
Todd <toddowenmpeli02@gmail.com>
2025-04-12 13:17:47 +0300

    Created a sitemap.ts, robot.ts, not-found page and error page. Also updated the home page content and favicon file.

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,41 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// Imports
+import {Metadata, NextPage} from "next";
+import {postType, flexibleContentType, pageType} from "@/context/pages";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+// Queries Functions
+import {getAllSeoContent} from "@/graphql/GetAllSeoContent";
+import {getAllFlexibleContentComponents} from "@/graphql/GetAllFlexibleContentComponents";
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
-}
+// Components
+import PageContextProvider from "@/context/providers/PageContextProvider";
+import RenderFlexibleContent from "@/components/FlexibleContent/RenderFlexibleContent";
+
+// Home Page Generated Metadata
+export const generateMetadata = async (): Promise<Metadata> => {
+	const seo: any = await getAllSeoContent(pageType?.home, postType?.pages);
+
+	return {
+		title: seo?.title,
+		description: seo?.metaDesc,
+	};
+};
+
+const HomePage: NextPage = async () => {
+	// Fetch priority content
+	const flexibleContentComponents: any = await getAllFlexibleContentComponents(
+		pageType?.home,
+		postType?.pages,
+		flexibleContentType?.pages
+	);
+
+	return (
+		<PageContextProvider
+			content={flexibleContentComponents?.content}
+			postTypeFlexibleContent={flexibleContentType?.pages}
+		>
+			<RenderFlexibleContent />
+		</PageContextProvider>
+	);
+};
+
+export default HomePage;
