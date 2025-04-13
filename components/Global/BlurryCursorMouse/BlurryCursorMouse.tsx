9429583ed500668235ad9cfe7e383946f3fc703c
Todd Mpeli <toddowenmpeli@rocketmail.com>
2025-04-13 21:37:22 +0300

    Updated project root entry content structure.

--- components/Global/BlurryCursorMouse/BlurryCursorMouse.tsx
+++ components/Global/BlurryCursorMouse/BlurryCursorMouse.tsx
@@ -60,7 +60,7 @@ const BlurryCursorMouse: FC = () => {
 					width: size,
 					height: size,
 					borderRadius: "100%",
-					backgroundColor: "#bff007",
+					backgroundColor: "#fa0008",
 					transition: `height 0.3s ease-out, width 0.3s ease-out, filter 0.3s ease-out`,
 				}}
 				className={styles.element}
