9740b6da31a08767eec51911024fc0117df24bb4
Todd Mpeli <toddowenmpeli@rocketmail.com>
2025-04-13 12:28:01 +0300

    Created a SmoothScrolling component functionality.

---
+++ components/Global/SmoothScrolling.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+// Imports
+import {FC} from "react";
+import {ReactLenis} from "lenis/react";
+
+export type ISmoothScrolling = {
+	children: React.ReactNode;
+};
+
+const SmoothScrolling: FC<ISmoothScrolling> = ({children}) => {
+	return (
+		<ReactLenis
+			root
+			options={{
+				// Learn more -> https://github.com/darkroomengineering/lenis?tab=readme-ov-file#instance-settings
+				lerp: 0.05,
+				//   infinite: true,
+				//   syncTouch: true,
+			}}
+		>
+			{children}
+		</ReactLenis>
+	);
+};
+
+export default SmoothScrolling;
